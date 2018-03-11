8483d84a9b28f7ee3fa22cf5f461e52a43d1a78a
Ayush Patel <25682019+ayush-patel@users.noreply.github.com>
2018-03-10 16:31:57 -0800

    Deployment Instructions

---
+++ aws-upload.conf.js
@@ -0,0 +1,12 @@
+module.exports = {
+	credentials:"aws-credentials.json",
+	bucketName:"ucla-eats.com",
+	patterns:[
+		"js/*.js",
+		"css/*.css",
+		"images/*.jpg",
+		"images/*.png",
+		"index.html",
+		"fonts/*"
+	]
+}
